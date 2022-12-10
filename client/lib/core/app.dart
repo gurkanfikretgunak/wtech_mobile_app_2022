df453be540f93e6d3ce345d0f028ccfd9899037c
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-12-11 01:17:16 +0300

    add : routes 💙

--- client/lib/core/app.dart
+++ client/lib/core/app.dart
@@ -1,3 +1,4 @@
+import 'package:client/core/routes/app_routest.dart';
 import 'package:client/core/views/sample_view/bloc/sample_bloc.dart';
 import 'package:client/core/views/sample_view/sample_view.dart';
 // ignore: depend_on_referenced_packages
@@ -13,6 +14,8 @@ class App extends StatelessWidget {
     return FlavorBanner(
       location: BannerLocation.topStart,
       child: MaterialApp(
+        onGenerateRoute: AppRoutes.instance.onGenerateRoute,
+
         // home: FlavorView(),
         home: Provider<MainBloc>(
           create: (_) => MainBloc(),
---
+++ client/lib/core/routes/app_routest.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+import '../enums/routes.enum.dart';
+
+class AppRoutes {
+  AppRoutes._init();
+  static final AppRoutes _instance = AppRoutes._init();
+  static AppRoutes get instance => _instance;
+
+  Route<dynamic>? onGenerateRoute(RouteSettings routeSettings) {
+    final routes = Routes.values.byName(routeSettings.name!);
+
+    switch (routes) {
+      case Routes.news:
+      // TODO: Handle this case.
+      case Routes.about:
+        // TODO: Handle this case.
+        break;
+      case Routes.blogs:
+        // TODO: Handle this case.
+        break;
+      case Routes.blogDetail:
+        // TODO: Handle this case.
+        break;
+      case Routes.educations:
+        // TODO: Handle this case.
+        break;
+      case Routes.educationDetail:
+        // TODO: Handle this case.
+        break;
+      case Routes.events:
+        // TODO: Handle this case.
+        break;
+      case Routes.eventDetail:
+        // TODO: Handle this case.
+        break;
+      case Routes.forgotPassword:
+        // TODO: Handle this case.
+        break;
+      case Routes.home:
+        // TODO: Handle this case.
+        break;
+      case Routes.jobs:
+        // TODO: Handle this case.
+        break;
+      case Routes.jobDetail:
+        // TODO: Handle this case.
+        break;
+      case Routes.mentors:
+        // TODO: Handle this case.
+        break;
+      case Routes.mentorDetail:
+        // TODO: Handle this case.
+        break;
+      case Routes.newsDetail:
+        // TODO: Handle this case.
+        break;
+      case Routes.onBoarding:
+        // TODO: Handle this case.
+        break;
+      case Routes.signin:
+        // TODO: Handle this case.
+        break;
+      case Routes.signup:
+        // TODO: Handle this case.
+        break;
+      case Routes.splash:
+        // TODO: Handle this case.
+        break;
+      case Routes.userProfile:
+        // TODO: Handle this case.
+        break;
+      case Routes.videos:
+        // TODO: Handle this case.
+        break;
+
+      default:
+        return navigate(
+          Text('No route defined for ${routes.name}'),
+        );
+    }
+    return null;
+  }
+
+  Route<dynamic>? navigate(Widget child) {
+    return MaterialPageRoute(
+      builder: (context) {
+        return child;
+      },
+    );
+  }
+}
