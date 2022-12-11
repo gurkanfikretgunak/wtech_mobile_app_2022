3e6051fc6b838d549ea8e0af37e27ac77fc89147
Ulaş Öztürk <ulasozturkdev@gmail.com>
2022-12-12 02:51:34 +0300

    add: custom widgets , constants, l10n, themedata

---
+++ client/lib/core/l10n/app_l10n_tr.dart
@@ -0,0 +1,282 @@
+import 'app_l10n.dart';
+
+/// The translations for Turkish (`tr`).
+class L10nTr extends L10n {
+  L10nTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get appBarTitle => 'İş ve Staj';
+
+  @override
+  String get signIn => 'Giriş Yap';
+
+  @override
+  String get email => 'E-Posta';
+
+  @override
+  String get signUp => 'Sign Up';
+
+  @override
+  String get view => 'View';
+
+  @override
+  String get categories => 'Categories';
+
+  @override
+  String get mentor => 'Mentor';
+
+  @override
+  String get news => 'Haberler';
+
+  @override
+  String get videos => 'Videos';
+
+  @override
+  String get jobAndinternship => 'Job and Internship';
+
+  @override
+  String get blog => 'Blog';
+
+  @override
+  String get currentTraining => 'Current Training';
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get search => 'Arama';
+
+  @override
+  String get events => 'Etkinlikler';
+
+  @override
+  String get trainings => 'Trainings';
+
+  @override
+  String get educationButton => 'Eğitime Git';
+
+  @override
+  String get aboutEducation => 'Eğitim Hakkında';
+
+  @override
+  String get participationConditions => 'Katılım Koşulları';
+
+  @override
+  String get gains => 'Kazanımlar';
+
+  @override
+  String get workingSpaces => 'Çalışma Alanları';
+
+  @override
+  String get applyButton => 'Başvur';
+
+  @override
+  String get terminatedButton => 'Sonlandı';
+
+  @override
+  String get internship => 'Staj';
+
+  @override
+  String get career => 'Kariyer';
+
+  @override
+  String get job => 'İş';
+
+  @override
+  String get lastPosts => 'Last Posts';
+
+  @override
+  String get views => 'views';
+
+  @override
+  String get nameSurnamee => 'Name-Surname';
+
+  @override
+  String get password => 'Şifre';
+
+  @override
+  String get userProfile => 'Profile (choose)';
+
+  @override
+  String get inspection => 'incele';
+
+  @override
+  String get apply => 'Başvur';
+
+  @override
+  String get skip => 'geç';
+
+  @override
+  String get continueNow => 'devam';
+
+  @override
+  String get searchInTheBlog => 'Blog içinde ara ';
+
+  @override
+  String get readMore => 'Daha fazlasını oku';
+
+  @override
+  String get shareThisPost => 'Bu gönderiyi paylaş';
+
+  @override
+  String get lastNews => 'Son Haberler';
+
+  @override
+  String get examine => 'İncele';
+
+  @override
+  String get signInGoogle => 'Google ile giriş yapın.';
+
+  @override
+  String get signInFacebook => 'Facebook ile giriş yapın.';
+
+  @override
+  String get signInGithub => 'Github ile giriş yapın.';
+
+  @override
+  String get signInApple => 'Apple ile giriş yapın.';
+
+  @override
+  String get signInEmail => 'E-posta ile giriş yapın.';
+
+  @override
+  String get loginBottomDescription => 'Kayıt Olarak Kullanıcı Sözleşmesini ve Aydınlatma Metnini Kabul Etmiş Olursunuz.';
+
+  @override
+  String get rememberMe => 'Beni Hatırla';
+
+  @override
+  String get forgotPassword => 'Parolanızı mı unuttunuz?';
+
+  @override
+  String get doYouHaveAccount => 'Üye değil misiniz? Buradan üye olun.';
+
+  @override
+  String get forgotPasswordDescription => 'Lütfen E-Postanızı veya telefon numaranızı giriniz.';
+
+  @override
+  String get backLogin => 'Girişe geri dön.';
+
+  @override
+  String get emailOrPhone => 'E-posta veya telefon';
+
+  @override
+  String get resetPassword => 'Şifreyi Yenile';
+
+  @override
+  String get name => 'İsim';
+
+  @override
+  String get surname => 'Soyisim';
+
+  @override
+  String get title => 'Ünvan';
+
+  @override
+  String get phoneNumber => 'Telefon numarası';
+
+  @override
+  String get birthday => 'Doğum Tarihi (gg/aa/yyyy)';
+
+  @override
+  String get tcNumber => 'T.C.No';
+
+  @override
+  String get selectGender => 'Cinsiyet seç';
+
+  @override
+  String get findaCause => 'Popüler Aramalar';
+
+  @override
+  String get upcomingEvents => 'En çok incelenenler';
+
+  @override
+  String get details => 'incele';
+
+  @override
+  String get myApplications => 'Başvurularım';
+
+  @override
+  String get myFavorites => 'Favorilerim';
+
+  @override
+  String get myProfile => 'Profilim';
+
+  @override
+  String get hello => 'merhaba';
+
+  @override
+  String get userName => 'Kullanıcı adı';
+
+  @override
+  String get help => 'yardım';
+
+  @override
+  String get logOut => 'Çıkış yap';
+
+  @override
+  String get trainingsIHaveApplied => 'Başvurduğum etkinlikler';
+
+  @override
+  String get activeTrainings => 'Aktif eğitimler';
+
+  @override
+  String get completedTutorials => 'Tamamladığım etkinlikler';
+
+  @override
+  String get mentorshipsIHaveAppliedFor => 'Başvurduğum mentörlükler';
+
+  @override
+  String get activeMentorings => 'Aktif mentörlükler';
+
+  @override
+  String get favorites => 'Favoriler';
+
+  @override
+  String get contact => 'İletişim';
+
+  @override
+  String get foundingMembers => 'Kurucu Üyeler';
+
+  @override
+  String get socialMedia => 'Sosyal Medya';
+
+  @override
+  String get workingHours => 'Çalışma Saatleri ';
+
+  @override
+  String get countributingInstitutions => 'Katkı Sağlayan Kurumlar';
+
+  @override
+  String get aboutUs => 'Hakkımızda';
+
+  @override
+  String get missionVision => 'Misyonumuz & Vizyonumuz';
+
+  @override
+  String get university => ' Üniversite ';
+
+  @override
+  String get department => ' Bölüm';
+
+  @override
+  String get startingDate => ' Başlangıç Tarihi ';
+
+  @override
+  String get endDate => ' Bitiş Tarihi ';
+
+  @override
+  String get jobStatus => 'İş Durumu ';
+
+  @override
+  String get position => 'Position';
+
+  @override
+  String get companyName => ' Firma Adı';
+
+  @override
+  String get profile => 'Profil';
+
+  @override
+  String get save => 'Kaydet';
+}
