0a47e408611e4d2ffc7be2de738c99fbf3cd2835
Elif Karagöz <75577514+eliffkaragoz@users.noreply.github.com>
2022-12-12 19:45:26 +0300

    add :  improving UI in news page

--- client/lib/core/views/news_view/news.view.dart
+++ client/lib/core/views/news_view/news.view.dart
@@ -1,13 +1,42 @@
+import 'package:client/core/extensions/extension.dart';
 import 'package:client/core/views/news_view/news.widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '../../l10n/app_l10n.dart';
+import '../common/widgets/custom_appbar.dart';
+import '../view_model_sample_two/sample_two.viewmodel.dart';
 
 class NewsView extends StatelessWidget with NewsWidgets {
-  const NewsView({super.key});
+  NewsView({super.key});
+
+  final _vm = GetIt.I.get<SampleViewModelTwo>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: body(context),
+      appBar: CustomAppBar.customAppBar(
+          context: context,
+          titleText: L10n.of(context)!.news,
+          isBackIcon: false),
+      body: Padding(
+        padding: context.paddingAll,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            card(isTitleCard: true),
+            titleText(context),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 2,
+                itemBuilder: (context, index) {
+                  return card();
+                },
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- client/lib/core/views/news_view/widgets/news_card.dart
+++ client/lib/core/views/news_view/widgets/news_card.dart
@@ -1,10 +1,72 @@
+import 'package:client/core/extensions/extension.dart';
+import 'package:client/core/views/news_view/news.widgets.dart';
 import 'package:flutter/material.dart';
 
-class NewsCard extends StatelessWidget {
-  const NewsCard({super.key});
+class NewsCard extends StatelessWidget with NewsWidgets {
+  const NewsCard({
+    super.key,
+    required this.title,
+    required this.subTitle,
+    required this.date,
+    this.isTitleCard,
+  });
+
+  final String title;
+  final String subTitle;
+  final String date;
+  final bool? isTitleCard;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      child: SizedBox(
+        child: isTitleCard != null
+            ? Wrap(
+                runSpacing: 10,
+                alignment: WrapAlignment.spaceEvenly,
+                children: [
+                  cardImage(imagePath: 'assets/images/title_card.png'),
+                  Padding(
+                    padding: context.paddingAll,
+                    child: Wrap(
+                      children: [
+                        cardTitleText(title, context),
+                        cardSubTitleText(subTitle, context),
+                        dateText(
+                          context: context,
+                          date: date,
+                          alignment: Alignment.bottomRight,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            : SizedBox(
+                child: Row(
+                  children: [
+                    Expanded(
+                        flex: 6,
+                        child:
+                            cardImage(imagePath: 'assets/images/news_1.png')),
+                    Expanded(
+                      flex: 6,
+                      child: Padding(
+                        padding: context.paddingAll,
+                        child: Wrap(
+                          runSpacing: 8,
+                          children: [
+                            cardTitleText(title, context),
+                            cardSubTitleText(subTitle, context),
+                            dateText(context: context, date: date),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
   }
 }
