231dac137a23fd16bebd2427cdab9eee927e6f25
Melih Çelik <melihcelik0909@gmail.com>
2022-12-15 18:02:02 +0300

    fix:mentors_detail completed 💙

--- client/lib/core/views/mentors_details_view/mentors_details.view.dart
+++ client/lib/core/views/mentors_details_view/mentors_details.view.dart
@@ -1,3 +1,4 @@
+import 'package:client/core/views/common/widgets/custom_appbar.dart';
 import 'package:client/core/views/mentors_details_view/mentors_details.viewmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -11,8 +12,10 @@ class MentorsDetailsView extends StatelessWidget with MentorDetailsWidgets {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: appBarTitle(context, 'Mentor Name')),
+      appBar: const CustomAppbar(titleText: 'Zehra Öney'),
       body: body(context),
+      floatingActionButton: floatingActionButton(context),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
