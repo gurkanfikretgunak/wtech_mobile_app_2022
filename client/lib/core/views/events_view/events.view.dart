f85230393bd11fece84dbf701724c065362dba50
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-12-18 21:28:12 +0300

    fix : Updated appBar using bt CustomAppBar

--- client/lib/core/views/events_view/events.view.dart
+++ client/lib/core/views/events_view/events.view.dart
@@ -1,11 +1,10 @@
 import 'package:client/core/extensions/extension.dart';
 import 'package:client/core/l10n/app_l10n.dart';
+import 'package:client/core/views/common/widgets/custom_appbar.dart';
 import 'package:client/core/views/events_view/widgets/calendar_listview.dart';
 import 'package:client/core/views/events_view/widgets/events_listview_builder.dart';
 import 'package:client/core/views/events_view/widgets/the_day_of_the_month.dart';
 import 'package:flutter/material.dart';
-
-import '../../constants/color_constans.dart';
 
 class EventsView extends StatefulWidget {
   const EventsView({super.key});
@@ -19,50 +18,10 @@ class _EventsViewState extends State<EventsView> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              debugPrint('Open drawer');
-            },
-            icon: const Icon(
-              Icons.menu,
-              size: 36,
-            )),
-        title: Padding(
-          padding: const EdgeInsets.only(top: 14.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(L10n.of(context)!.events,
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      ?.copyWith(fontSize: 30, fontWeight: FontWeight.normal)),
-            ],
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: context.onlyTopPaddingNormal,
-            child: Container(
-                height: 40,
-                width: 85,
-                decoration: BoxDecoration(
-                    color: ColorConstant.instance.yellow,
-                    borderRadius: BorderRadius.circular(50)),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("W-27",
-                        style: Theme.of(context)
-                            .textTheme
-                            .subtitle1
-                            ?.copyWith(fontWeight: FontWeight.bold)),
-                    const CircleAvatar(maxRadius: 18),
-                  ],
-                )),
-          )
-        ],
+      appBar: CustomAppbar(
+        titleText: L10n.of(context)!.events,
+        isCheck: true,
+        isName: false,
       ),
       body: Padding(
         padding: context.horizontalPaddingNormal,
