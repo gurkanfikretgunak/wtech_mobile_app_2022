cfd9c3d41877d662e538caffe56d934b048a732f
Aysenur Perker <nurperker15@gmail.com>
2022-12-19 17:57:44 +0300

    add: Help,Mission& Vision Page

--- client/lib/core/di/config/configure_dependencies.config.dart
+++ client/lib/core/di/config/configure_dependencies.config.dart
@@ -6,14 +6,14 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:client/core/data/network/abstractions/todos_service.dart'
-    as _i28;
+    as _i29;
 import 'package:client/core/data/network/abstractions/users_service.dart'
-    as _i30;
-import 'package:client/core/data/network/services/todos/todo_api.dart' as _i29;
-import 'package:client/core/data/network/services/users/user_api.dart' as _i31;
-import 'package:client/core/di/config/app_config.dart' as _i26;
-import 'package:client/core/di/config/raw_helper.dart' as _i18;
-import 'package:client/core/di/config/register_module.dart' as _i32;
+    as _i31;
+import 'package:client/core/data/network/services/todos/todo_api.dart' as _i30;
+import 'package:client/core/data/network/services/users/user_api.dart' as _i32;
+import 'package:client/core/di/config/app_config.dart' as _i27;
+import 'package:client/core/di/config/raw_helper.dart' as _i19;
+import 'package:client/core/di/config/register_module.dart' as _i33;
 import 'package:client/core/views/applys_view/applys.viewmodel.dart' as _i3;
 import 'package:client/core/views/authentication/authentication.viewmodel.dart'
     as _i4;
@@ -28,29 +28,30 @@ import 'package:client/core/views/forgot_password_view/fotgotpass.viewmodel.dart
     as _i9;
 import 'package:client/core/views/founding_members_view/founding.members.viewmodel.dart'
     as _i10;
-import 'package:client/core/views/home_view/home.viewmodel.dart' as _i11;
+import 'package:client/core/views/help_view/help.viewmodel.dart' as _i11;
+import 'package:client/core/views/home_view/home.viewmodel.dart' as _i12;
 import 'package:client/core/views/jobs_details_view/jobs_detail.viewmodel.dart'
-    as _i12;
-import 'package:client/core/views/jobs_view/jobs.viewmodel.dart' as _i13;
+    as _i13;
+import 'package:client/core/views/jobs_view/jobs.viewmodel.dart' as _i14;
 import 'package:client/core/views/mentors_details_view/mentors_details.viewmodel.dart'
-    as _i14;
-import 'package:client/core/views/mentors_view/mentors.viewmodel.dart' as _i15;
+    as _i15;
+import 'package:client/core/views/mentors_view/mentors.viewmodel.dart' as _i16;
 import 'package:client/core/views/news_detail_view/news_detail.viewmodel.dart'
-    as _i16;
-import 'package:client/core/views/news_view/news.viewmodel.dart' as _i17;
+    as _i17;
+import 'package:client/core/views/news_view/news.viewmodel.dart' as _i18;
 import 'package:client/core/views/settings_view/settings.viewmodel.dart'
-    as _i21;
-import 'package:client/core/views/signup_view/signup.viewmodel.dart' as _i22;
+    as _i22;
+import 'package:client/core/views/signup_view/signup.viewmodel.dart' as _i23;
 import 'package:client/core/views/supporting_institutions_view/support_inst.viewmodel.dart'
-    as _i23;
-import 'package:client/core/views/user_profile_view/user_profile.viewmodel.dart'
     as _i24;
-import 'package:client/core/views/videos_view/videos.viewmodel.dart' as _i25;
+import 'package:client/core/views/user_profile_view/user_profile.viewmodel.dart'
+    as _i25;
+import 'package:client/core/views/videos_view/videos.viewmodel.dart' as _i26;
 import 'package:client/core/views/view_model_sample/sample.viewmodel.dart'
-    as _i19;
-import 'package:client/core/views/view_model_sample_two/sample_two.viewmodel.dart'
     as _i20;
-import 'package:dio/dio.dart' as _i27;
+import 'package:client/core/views/view_model_sample_two/sample_two.viewmodel.dart'
+    as _i21;
+import 'package:dio/dio.dart' as _i28;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart'
     as _i2; // ignore_for_file: unnecessary_lambdas
@@ -78,37 +79,38 @@ Future<_i1.GetIt> $initGetIt(
   gh.factory<_i9.ForgotPasswordViewModel>(() => _i9.ForgotPasswordViewModel());
   gh.factory<_i10.FoundingMembersViewModel>(
       () => _i10.FoundingMembersViewModel());
-  gh.factory<_i11.HomeViewModel>(() => _i11.HomeViewModel());
-  gh.factory<_i12.JobsDetailViewModel>(() => _i12.JobsDetailViewModel());
-  gh.factory<_i13.JobsViewModel>(() => _i13.JobsViewModel());
-  gh.factory<_i14.MentorsDetailsViewModel>(
-      () => _i14.MentorsDetailsViewModel());
-  gh.factory<_i15.MentorsViewModel>(() => _i15.MentorsViewModel());
-  gh.factory<_i16.NewsDetailViewModel>(() => _i16.NewsDetailViewModel());
-  gh.factory<_i17.NewsViewModel>(() => _i17.NewsViewModel());
-  await gh.singletonAsync<_i18.RawConfig>(
+  gh.factory<_i11.HelpViewModel>(() => _i11.HelpViewModel());
+  gh.factory<_i12.HomeViewModel>(() => _i12.HomeViewModel());
+  gh.factory<_i13.JobsDetailViewModel>(() => _i13.JobsDetailViewModel());
+  gh.factory<_i14.JobsViewModel>(() => _i14.JobsViewModel());
+  gh.factory<_i15.MentorsDetailsViewModel>(
+      () => _i15.MentorsDetailsViewModel());
+  gh.factory<_i16.MentorsViewModel>(() => _i16.MentorsViewModel());
+  gh.factory<_i17.NewsDetailViewModel>(() => _i17.NewsDetailViewModel());
+  gh.factory<_i18.NewsViewModel>(() => _i18.NewsViewModel());
+  await gh.singletonAsync<_i19.RawConfig>(
     () => registerModule.config(),
     preResolve: true,
   );
-  gh.factory<_i19.SampleViewModel>(() => _i19.SampleViewModel());
-  gh.factory<_i20.SampleViewModelTwo>(() => _i20.SampleViewModelTwo());
-  gh.factory<_i21.SettingsViewModel>(() => _i21.SettingsViewModel());
-  gh.factory<_i22.SignUpViewModel>(() => _i22.SignUpViewModel());
-  gh.factory<_i23.SupportInstutionsViewModel>(
-      () => _i23.SupportInstutionsViewModel());
-  gh.factory<_i24.UserProfileViewModel>(() => _i24.UserProfileViewModel());
-  gh.factory<_i25.VideosViewModel>(() => _i25.VideosViewModel());
-  gh.singleton<_i26.AppConfig>(_i26.AppConfig(get<_i18.RawConfig>()));
-  gh.singleton<_i27.Dio>(registerModule.dio(get<_i26.AppConfig>()));
-  gh.factory<_i28.TodosService>(() => _i29.RestTodoService(
-        get<_i27.Dio>(),
-        get<_i26.AppConfig>(),
+  gh.factory<_i20.SampleViewModel>(() => _i20.SampleViewModel());
+  gh.factory<_i21.SampleViewModelTwo>(() => _i21.SampleViewModelTwo());
+  gh.factory<_i22.SettingsViewModel>(() => _i22.SettingsViewModel());
+  gh.factory<_i23.SignUpViewModel>(() => _i23.SignUpViewModel());
+  gh.factory<_i24.SupportInstutionsViewModel>(
+      () => _i24.SupportInstutionsViewModel());
+  gh.factory<_i25.UserProfileViewModel>(() => _i25.UserProfileViewModel());
+  gh.factory<_i26.VideosViewModel>(() => _i26.VideosViewModel());
+  gh.singleton<_i27.AppConfig>(_i27.AppConfig(get<_i19.RawConfig>()));
+  gh.singleton<_i28.Dio>(registerModule.dio(get<_i27.AppConfig>()));
+  gh.factory<_i29.TodosService>(() => _i30.RestTodoService(
+        get<_i28.Dio>(),
+        get<_i27.AppConfig>(),
       ));
-  gh.factory<_i30.UserService>(() => _i31.RestUserService(
-        get<_i27.Dio>(),
-        get<_i26.AppConfig>(),
+  gh.factory<_i31.UserService>(() => _i32.RestUserService(
+        get<_i28.Dio>(),
+        get<_i27.AppConfig>(),
       ));
   return get;
 }
 
-class _$RegisterModule extends _i32.RegisterModule {}
+class _$RegisterModule extends _i33.RegisterModule {}
---
+++ client/lib/core/views/help_view/help.widget.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: dead_code
+
+import 'package:client/core/views/common/widgets/text/custom_text.dart';
+import 'package:client/core/views/help_view/help.viewmodel.dart';
+import 'package:client/core/views/settings_view/settings.viewmodel.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import '../../constants/color_constans.dart';
+import '../common/widgets/custom_card.dart';
+
+class HelpViewWidget {
+  Widget helpExpansionCard(BuildContext context, String expansionText) {
+    // ignore: no_leading_underscores_for_local_identifiers
+    bool _customTileExpanded = false;
+    final _vm = GetIt.I.get<HelpViewModel>();
+    return CustomCard(
+      color: ColorConstant.instance.shinyWhite,
+      elevation: 1.5,
+      child: StreamBuilder(
+          stream: _vm.expansion,
+          builder: (context, snapshot) {
+            return ExpansionTile(
+              maintainState: snapshot.data ?? "",
+              title: CustomText(
+                expansionText,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+              trailing: Icon(
+                _customTileExpanded ? Icons.arrow_drop_down_circle : Icons.arrow_drop_down,
+              ),
+              children: const <Widget>[
+                ListTile(title: Text('This is tile number 2')),
+              ],
+              onExpansionChanged: (bool expanded) {
+                _vm.changeExpansion();
+              },
+            );
+          }),
+    );
+  }
+}
