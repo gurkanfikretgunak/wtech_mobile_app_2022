6043aba0a193cb43ec465fefd9dc5bcad1a9aac6
Aysenur Perker <nurperker15@gmail.com>
2022-12-19 21:03:28 +0300

    Merge branch 'dev' of

--- client/lib/core/app.dart
+++ client/lib/core/app.dart
@@ -1,11 +1,12 @@
 import 'package:client/core/l10n/app_l10n.dart';
 import 'package:client/core/routes/app_routest.dart';
+import 'package:client/core/themes/change_theme/change_theme.viewmodel.dart';
 import 'package:client/core/themes/custom_theme.dart';
-import 'package:client/core/views/bottom_navbar/bottom_navbar.view.dart';
 import 'package:client/core/views/sample_view/bloc/sample_bloc.dart';
 import 'package:client/core/views/splash_view/splash.view.dart';
 import 'package:flavor/flavor.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 // ignore: depend_on_referenced_packages
 import 'package:provider/provider.dart';
 
@@ -14,21 +15,27 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final _vm = GetIt.I.get<ChangeThemeViewModel>();
+
     return FlavorBanner(
       location: BannerLocation.topStart,
-      child: MaterialApp(
-        localizationsDelegates: L10n.localizationsDelegates,
-        supportedLocales: L10n.supportedLocales,
-        onGenerateRoute: AppRoutes.instance.onGenerateRoute,
-        home: Provider<MainBloc>(
-          create: (_) => MainBloc(),
-          dispose: (context, bloc) => bloc.dispose(),
-          child: SplashView(),
-        ),
-        title: "Flutter Boilerplate : ${Flavor.I.getString(Keys.appTitle)}",
-        theme: CustomTheme.customThemeData(context),
-        debugShowCheckedModeBanner: false,
-      ),
+      child: StreamBuilder(
+          stream: _vm.theme,
+          builder: (context, snapshot) {
+            return MaterialApp(
+              localizationsDelegates: L10n.localizationsDelegates,
+              supportedLocales: L10n.supportedLocales,
+              onGenerateRoute: AppRoutes.instance.onGenerateRoute,
+              home: Provider<MainBloc>(
+                create: (_) => MainBloc(),
+                dispose: (context, bloc) => bloc.dispose(),
+                child: SplashView(),
+              ),
+              title: "Flutter Boilerplate : ${Flavor.I.getString(Keys.appTitle)}",
+              theme: snapshot.data! ? CustomTheme.customLightTheme(context) : CustomTheme.customDarkTheme(context),
+              debugShowCheckedModeBanner: false,
+            );
+          }),
     );
   }
 }
