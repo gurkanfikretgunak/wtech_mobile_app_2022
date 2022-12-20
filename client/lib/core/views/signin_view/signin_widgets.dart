3e198e692140a5b08a6d3c641caab2e70665d09f
ONUR ŞAHİN <1803022060@student.beykent.edu.tr>
2022-12-21 00:28:09 +0300

    fix: Signin Accounts View Icons with Asset Gen

--- client/lib/core/views/signin_view/signin_widgets.dart
+++ client/lib/core/views/signin_view/signin_widgets.dart
@@ -2,7 +2,9 @@ import 'package:client/core/l10n/app_l10n.dart';
 import 'package:client/core/views/authentication/authentication.viewmodel.dart';
 import 'package:client/core/views/common/widgets/text/custom_textfield.dart';
 import 'package:client/core/views/signin_view/widgets/sigin_account_button_widget.dart';
+import 'package:client/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get_it/get_it.dart';
 
 class SignInWidget {
@@ -52,22 +54,22 @@ class SignInWidget {
     return Column(
       children: [
         SignInAccountButton(
-          icon: "assets/images/google.svg",
+          icon: Assets.images.google,
           text: L10n.of(context)!.signinWith,
           socialText: "",
         ),
         SignInAccountButton(
-          icon: "assets/images/facebook.svg",
+          icon: Assets.images.facebook,
           text: L10n.of(context)!.signinWith,
           socialText: "",
         ),
         SignInAccountButton(
-          icon: "assets/images/apple.svg",
+          icon: Assets.images.apple,
           text: L10n.of(context)!.signinWith,
           socialText: "",
         ),
         SignInAccountButton(
-          icon: "assets/images/github.svg",
+          icon: Assets.images.github,
           text: L10n.of(context)!.signinWith,
           socialText: "",
         ),
