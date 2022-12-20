b673fcf9c2c2413c8f2ae1613329d8b2cb2dd1cf
Batuhan-Akdemirr <85792583+Batuhan-Akdemirr@users.noreply.github.com>
2022-12-20 16:21:16 +0300

    fix : Event view

--- client/lib/core/views/events_detail_view/widgets/events_detail.dart
+++ client/lib/core/views/events_detail_view/widgets/events_detail.dart
@@ -16,50 +16,53 @@ class EventsDetail extends StatefulWidget {
 class _EventsDetailState extends State<EventsDetail> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Stack(children: [
-            const EventsImage(),
-            Positioned(
-                right: 0,
-                bottom: 10,
-                child: EventsApplyButton(
-                  onPressed: () {
-                    debugPrint('Apply events');
-                  },
-                )),
-          ]),
-        ),
-        Padding(
-          padding: context.onlyTopPaddingNormal * 1.5,
-          child: Text(
-            'İstanbul Teknolojide Lider Kadınlar Eğitim Programı',
-            style: Theme.of(context).textTheme.headline6,
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: Stack(children: [
+              const EventsImage(),
+              Positioned(
+                  right: context.dynamicWidth(0.01),
+                  bottom: MediaQuery.of(context).size.height * 0.015,
+                  child: EventsApplyButton(
+                    onPressed: () {
+                      debugPrint('Apply events');
+                    },
+                  )),
+            ]),
           ),
-        ),
-        Padding(
-          padding: context.onlyTopPaddingNormal * 1.5,
-          child: Text(
-            'Wtech Akademi',
-            style: Theme.of(context)
-                .textTheme
-                .headline5
-                ?.copyWith(color: ColorConstant.instance.blue),
+          Padding(
+            padding: context.onlyTopPaddingNormal * 1.5,
+            child: Text(
+              'İstanbul Teknolojide Lider Kadınlar Eğitim Programı',
+              style: Theme.of(context).textTheme.headline6,
+            ),
           ),
-        ),
-        Padding(
-          padding: context.verticalPaddingNormal * 1.5,
-          child: Text(
-            'Teknolojide Kadın Derneği olarak organizasyon partneri olduğumuz Teknoloji Buluşmaları’nda her ilde 200 kadına teknoloji okuryazarı olmaları adına vizyon eğitimleri vereceğiz. Vizyon eğitimi alan kadınlar arasından teknoloji konusunda yetenekli olanlara ayrıca online teknik uzmanlık eğitimleri başlatacağız.',
-            style: Theme.of(context)
-                .textTheme
-                .subtitle2
-                ?.copyWith(color: Colors.grey.withOpacity(0.5)),
+          Padding(
+            padding: context.onlyTopPaddingNormal * 1.5,
+            child: Text(
+              'Wtech Akademi',
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5
+                  ?.copyWith(color: ColorConstant.instance.blue),
+            ),
           ),
-        )
-      ],
+          Padding(
+            padding: context.verticalPaddingNormal * 1.5,
+            child: Text(
+              'Teknolojide Kadın Derneği olarak organizasyon partneri olduğumuz Teknoloji Buluşmaları’nda her ilde 200 kadına teknoloji okuryazarı olmaları adına vizyon eğitimleri vereceğiz. Vizyon eğitimi alan kadınlar arasından teknoloji konusunda yetenekli olanlara ayrıca online teknik uzmanlık eğitimleri başlatacağız.',
+              style: Theme.of(context)
+                  .textTheme
+                  .subtitle2
+                  ?.copyWith(color: Colors.grey.withOpacity(0.5)),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- client/lib/core/views/events_view/widgets/events.dart
+++ client/lib/core/views/events_view/widgets/events.dart
@@ -1,4 +1,5 @@
 import 'package:client/core/constants/color_constans.dart';
+import 'package:client/core/views/events_detail_view/widgets/base_main_sheet.dart';
 import 'package:client/core/views/events_view/events_image.dart';
 import 'package:client/core/views/events_view/widgets/events_description.dart';
 import 'package:flutter/material.dart';
@@ -12,29 +13,46 @@ class Events extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.4,
-      child: Card(
-        color: ColorConstant.instance.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Expanded(
-              flex: 6,
-              child: EventsImage(),
-            ),
-            Expanded(
-              flex: 4,
-              child: Padding(
-                padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).size.width * 0.025),
-                child: const EventsDescription(),
+      child: InkWell(
+        onTap: () {
+          _navigateEventDetails(context);
+        },
+        child: Card(
+          color: ColorConstant.instance.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Expanded(
+                flex: 6,
+                child: EventsImage(),
               ),
-            )
-          ],
+              Expanded(
+                flex: 4,
+                child: Padding(
+                  padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).size.width * 0.025),
+                  child: const EventsDescription(),
+                ),
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void _navigateEventDetails(BuildContext context) {
+     showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      builder: (context) {
+        return const BaseMainSheet();
+      },
     );
   }
 }
