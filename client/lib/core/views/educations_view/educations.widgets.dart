b48a19d0c4367af0672735d67644fef833fc9ef0
ArdaNutyali <64331573+ArdaNutyali@users.noreply.github.com>
2022-12-22 18:26:46 +0300

    fix: Education Search Bar Updated

--- client/lib/core/views/educations_view/educations.widgets.dart
+++ client/lib/core/views/educations_view/educations.widgets.dart
@@ -1,10 +1,8 @@
-import 'package:client/core/utils/constants/colors/color_constans.dart';
 import 'package:client/core/utils/extensions/common_extension.dart';
 import 'package:client/core/l10n/app_l10n.dart';
 import 'package:client/core/views/educations_view/widgets/education_card.dart';
 import 'package:client/core/views/educations_view/widgets/search_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:logger/logger.dart';
 import 'package:client/core/views/common/widgets/text/custom_textfield.dart';
 
 class EducationsWidgets {
@@ -17,28 +15,12 @@ class EducationsWidgets {
   }
 
   Widget searchEducationWidget(BuildContext context) {
-    return FittedBox(
-      child: Row(
-        children: [
-          SizedBox(
-            height: context.dynamicHeight(0.055),
-            width: context.dynamicWidth(0.75),
-            child: CustomTextFormField(
-              prefixIcon: const Icon(Icons.search),
-              labelText: L10n.of(context)!.search,
-            ),
-          ),
-          IconButton(
-            onPressed: () {
-              Logger().d("message");
-            },
-            icon: Icon(
-              Icons.filter_list_outlined,
-              size: 30,
-              color: ColorConstant.instance.blue,
-            ),
-          )
-        ],
+    return Padding(
+      padding: context.paddingLow,
+      child: CustomTextFormField(
+        prefixIcon: const Icon(Icons.search),
+        labelText: L10n.of(context)!.search,
+        isSearch: true,
       ),
     );
   }
