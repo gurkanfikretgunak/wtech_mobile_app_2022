543fec18806d6526cb0f89859f879c1046b06101
Gürkan Fikret Günak <52853374+gurkanfikretgunak@users.noreply.github.com>
2022-12-25 16:49:17 +0300

    upgrade: baseScaffold method and Videos View

--- client/lib/core/views/abstractions/base_view_model.dart
+++ client/lib/core/views/abstractions/base_view_model.dart
@@ -1,11 +1,28 @@
 // ignore: depend_on_referenced_packages
+import 'package:flutter/material.dart';
 import 'package:rxdart/subjects.dart';
 
 abstract class BaseViewModel {
   final _loading = BehaviorSubject<bool>.seeded(false);
-
   Stream get loading => _loading.stream;
   void setLoading(bool value) => _loading.add(value);
+
+  Widget baseScaffold(
+    BuildContext context, {
+    Widget? body,
+    PreferredSizeWidget? appbar,
+    Widget? navbar,
+    Widget? drawer,
+    Widget? fab,
+  }) {
+    return Scaffold(
+      body: body,
+      appBar: appbar,
+      bottomNavigationBar: navbar,
+      drawer: drawer,
+      floatingActionButton: fab,
+    );
+  }
 
   void clear();
 }
--- client/lib/core/views/videos_view/videos.widgets.dart
+++ client/lib/core/views/videos_view/videos.widgets.dart
@@ -1,12 +1,22 @@
 import 'package:chewie/chewie.dart';
 import 'package:client/core/utils/extensions/common_extension.dart';
 import 'package:client/core/l10n/app_l10n.dart';
+import 'package:client/core/views/common/widgets/custom_appbar.dart';
 import 'package:client/core/views/common/widgets/text/custom_text.dart';
 import 'package:client/core/views/videos_view/widgets/card_item_widget.dart';
 import 'package:flutter/material.dart';
 
 class VideosWidgets {
   late ChewieController chewieController;
+
+  PreferredSizeWidget appbar(BuildContext context) {
+    return CustomAppbar(
+      titleText: L10n.of(context)!.videos,
+      isCheck: false,
+      isName: false,
+    );
+  }
+
   Widget body(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
