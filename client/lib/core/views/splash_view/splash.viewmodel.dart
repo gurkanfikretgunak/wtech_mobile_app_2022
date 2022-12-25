fcb12894aef3349a391f6f482155c9020b05d549
cantanrikulu <73619391+cantanrikulu@users.noreply.github.com>
2022-12-25 17:32:23 +0300

    upgrade: splash view

--- client/lib/core/views/splash_view/splash.viewmodel.dart
+++ client/lib/core/views/splash_view/splash.viewmodel.dart
@@ -1,8 +1,12 @@
+import 'package:client/core/views/abstractions/base_view_model.dart';
 import 'package:injectable/injectable.dart';
 
 @Injectable()
-class SplashViewModel {
+class SplashViewModel extends BaseViewModel {
   splashDelay() {
     Future.delayed(const Duration(milliseconds: 3000), () {});
   }
+
+  @override
+  void clear() {}
 }
