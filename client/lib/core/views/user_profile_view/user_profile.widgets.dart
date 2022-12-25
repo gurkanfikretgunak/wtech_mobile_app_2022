bdb737faadfe12cc483281db0edf2fb7f0ac55f5
Muhammed18-09 <79153020+Muhammed18-09@users.noreply.github.com>
2022-12-25 17:43:43 +0300

    upgrade: User profile view

--- client/lib/core/views/user_profile_view/user_profile.widgets.dart
+++ client/lib/core/views/user_profile_view/user_profile.widgets.dart
@@ -1,4 +1,6 @@
+import 'package:client/core/l10n/app_l10n.dart';
 import 'package:client/core/utils/extensions/common_extension.dart';
+import 'package:client/core/views/common/widgets/custom_appbar.dart';
 import 'package:client/core/views/user_profile_view/widgets/profil_item.dart';
 import 'package:client/core/views/user_profile_view/widgets/profil_item_icon_list.dart';
 import 'package:flutter/material.dart';
@@ -58,6 +60,45 @@ class UserProfileWidgets {
         ],
       ),
     );
+  }
+
+ PreferredSizeWidget appbar(BuildContext context) {
+    return CustomAppbar(
+      titleText: L10n.of(context)!.profile,
+      isName: false,
+      isCheck: false,
+    );
+  }
+  Widget body(BuildContext context) {
+    return Wrap(children: [
+      Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+        Stack(children: [
+          Padding(
+            padding: context.onlyTopPaddingHigh,
+            child: Container(
+                height: context.dynamicHeight(0.75),
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color.fromARGB(255, 245, 242, 242),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(60),
+                      topRight: Radius.circular(60)),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    userNameText(context),
+                    const CustomText("gurkan@gmail.com"),
+                    context.emptySizedHeightBoxHigh,
+                    context.emptySizedHeightBoxNormal,
+                    listViewBuildProfilItem(context),
+                  ],
+                )),
+          ),
+          profileAvatar(context)
+        ]),
+      ]),
+    ]);
   }
 
   Widget userNameText(BuildContext context) {
