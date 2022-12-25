33d1887bb69b0c80f76e8e5c3a39b9c92efd4557
Melih Çelik <melihcelik0909@gmail.com>
2022-12-25 23:19:33 +0300

    fix: scaffold updated to baseScaffold

--- client/lib/core/views/mentors_view/mentors.view.dart
+++ client/lib/core/views/mentors_view/mentors.view.dart
@@ -1,5 +1,3 @@
-import 'package:client/core/l10n/app_l10n.dart';
-import 'package:client/core/views/common/widgets/custom_appbar.dart';
 import 'package:client/core/views/mentors_view/mentors.viewmodel.dart';
 import 'package:client/core/views/mentors_view/mentors.widgets.dart';
 import 'package:flutter/material.dart';
@@ -10,15 +8,9 @@ class MentorsView extends StatelessWidget with MentorsWidgets {
   final _vm = GetIt.I.get<MentorsViewModel>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //Will be change with common appbar
-      appBar: CustomAppbar(
-        titleText: L10n.of(context)!.mentorAppBarTitle,
-        isName: false,
-        isCheck: false,
-      ),
-
-      drawer: const Drawer(child: Text("data")),
+    return _vm.baseScaffold(
+      context,
+      appbar: appbar(context),
       body: body(context),
     );
   }
